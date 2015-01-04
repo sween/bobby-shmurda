ca1262fdd76bbd68255cba96509d4ac2220d45db
sween <sween@Integration-Required-3.local>
2015-01-03 20:56:52 -0500

    cleaned up Ruby

--- ruby/bobbyshmurda.rb
+++ ruby/bobbyshmurda.rb
@@ -3,9 +3,9 @@ class BobbyShmurda
       timenow = Time.now  
       weekago = timenow -604800 
       # "Weekday Month DayofMonth, Year" // Ex. Tuesday September 11, 2001
-     return weekago
+     return weekago.strftime("%A %B %d, %Y")
   end
 end
 
 gar = BobbyShmurda.new
-puts gar.about_a_week_ago.strftime("%A %B %d, %Y")
+puts gar.about_a_week_ago
